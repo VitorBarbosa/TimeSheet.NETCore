7e447887e8aa0945c8f599eaa6874abc383861c3
Vitor Mascena Barbosa <vitormbarbosa@gmail.com>
2016-10-05 20:15:12 -0300

    Added method to calculate resulting timespan

--- src/util/Time.cs
+++ src/util/Time.cs
@@ -3,6 +3,13 @@ using System.Globalization;
 
 namespace TimeSheet.src.util {
 	public class Time {
+		/// <summary>
+		/// Parses a string into a TimeSpan with the desired format string
+		/// </summary>
+		/// <param name="timeSpanString"></param>
+		/// <param name="timeSpan"></param>
+		/// <param name="format"></param>
+		/// <returns>True if string is able to be parsed into a TimeSpan</returns>
 		public static bool ParseTimeSpan(string timeSpanString, out TimeSpan timeSpan, string format = "c") {
             var cultureInfo = CultureInfo.CurrentCulture;
 
@@ -12,5 +19,19 @@ namespace TimeSheet.src.util {
 
             return false;
         }
+
+		public static TimeSpan CalculateResultingTimeSpan(params TimeSpan[] timeSpans) {
+			if (timeSpans.Length % 2 != 0) {
+				throw new ArgumentException("Odd number of timeSpans");
+			}
+
+			var result = new TimeSpan();
+
+			for (var i = 0; i < timeSpans.Length; i += 2) {
+				result = timeSpans[i+1].Subtract(timeSpans[i]);
+			}
+
+			return result;
+		}
 	}
 }
